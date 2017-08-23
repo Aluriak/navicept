a430cdb317e1a731843577ba994420de51968692
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-08-23 19:34:13 +0200

    gui: use the new navigation API + messages to user

--- gui.py
+++ gui.py
@@ -5,6 +5,7 @@ a tkinter gui.
 
 from functools import partial
 import tkinter as tk
+from tkinter import font
 
 import navigation
 from context import Context
@@ -12,6 +13,13 @@ from navigation import find_concepts_interactively
 
 
 DEFAULT_WM_TITLE = 'floating_navicept'
+NO_COLOR = 'white'
+COLOR_BG_TEXT = 'light grey'
+COLOR_UNSET = 'pink'
+COLOR_ERR = 'red'
+COLOR_LOG = 'dark green'
+COLOR_OK = 'light green'
+COLOR_WAITING = '#fd6b14'
 
 
 class Application(tk.Frame):
@@ -34,6 +42,8 @@ class Application(tk.Frame):
             child.destroy()
         buttons = {}
         for idx, dim in enumerate(self.context.sets):
+            requireds, forbiddens = self.constraints[idx]
+            user_requireds, user_forbiddens = self.user_constraints[idx]
             dim_buttons = tk.Frame(master=self)
             for elem in sorted(dim, reverse=True):
                 button = tk.Button(dim_buttons, text=str(elem))
@@ -42,14 +52,20 @@ class Application(tk.Frame):
                 button.bind('<Button-2>', partial(self.choose_elem, dim_idx=idx, elem=elem, decision=''))
                 button.bind('<Button-3>', partial(self.choose_elem, dim_idx=idx, elem=elem, decision='out'))
                 # color
-                required = any(elem in cons.required for cons in self.constraints)
-                forbidden = any(elem in cons.forbidden for cons in self.constraints)
+                required = elem in requireds
+                forbidden = elem in forbiddens
+                user_required = elem in user_requireds
+                user_forbidden = elem in user_forbiddens
                 if forbidden and required:
                     raise ValueError("Element {} is both required and forbidden. That's unexpected.".format(elem))
-                if required:
+                if user_required:
                     button['background'] = 'green'
-                elif forbidden:
+                elif required:
+                    button['background'] = 'light green'
+                elif user_forbidden:
                     button['background'] = 'red'
+                elif forbidden:
+                    button['background'] = 'pink'
                 buttons[idx, elem] = button
                 button.pack(side='right')
             dim_buttons.pack()
@@ -61,10 +77,38 @@ class Application(tk.Frame):
                               command=self.master.destroy)
         self.previous = tk.Button(self, text="", fg="green",
                                   command=self.restore_previous)
+        self.current_error = tk.StringVar(self, value=' ' * 40)
+        self.logline = tk.Label(self, textvariable=self.current_error)
         self.quit.pack(side="bottom")
         self.reset.pack(side="bottom")
         self.previous.pack(side="bottom")
+
+        # logging for user
+        error_font = font.Font(family='TkFixedFont', size=10, weight='bold')
+        self.current_error = tk.StringVar(value=' ' * 40)
+        self.lab_error = tk.Label(self, textvariable=self.current_error, fg=COLOR_ERR, font=error_font)
+        self.lab_error.pack(side="bottom")
+
         self.pack()
+
+
+    def err(self, msg:str):
+        """Report given error message to user"""
+        self.lab_error.configure(fg=COLOR_ERR)
+        self.current_error.set(str(msg))
+        self.update_idletasks()  # redraw (do not wait for the end of event handling)
+
+    def log(self, msg:str):
+        """Report given log message to user"""
+        self.lab_error.configure(fg=COLOR_LOG)
+        self.current_error.set(str(msg))
+        self.update_idletasks()
+
+    def info(self, msg:str):
+        """Report given log message to user"""
+        self.lab_error.configure(fg=COLOR_WAITING)
+        self.current_error.set(str(msg))
+        self.update_idletasks()
 
 
     def reset_constraints(self):
@@ -72,16 +116,19 @@ class Application(tk.Frame):
         initialize a search, and each time user ask to reset.
 
         """
-        print('STARTING CONCEPT SEARCH…')
-        # self.coroutine = self.user_choice(find_concepts_interactively(self.context))
         self.coroutine = find_concepts_interactively(self.context)
-        self.context, self.constraints = next(self.coroutine)
-        self.create_widgets()
+        try:
+            self.user_constraints, self.constraints = next(self.coroutine)
+        except StopIteration as last:
+            self.show_found_concept(last.value)
+        else:
+            self.create_widgets()
 
 
     def restore_previous(self):
         """Restore the previous state"""
-        raise NotImplementedError
+        idx, elem, decision = self._last_user_input
+        self.choose_elem(None, idx, elem, '')
 
 
     def choose_elem(self, event, dim_idx:set, elem:str, decision:str):
@@ -89,18 +136,17 @@ class Application(tk.Frame):
         of the searched concept.
 
         """
+        self._last_user_input = dim_idx, elem, decision
+        # self.info('STARTING CONCEPT SEARCH…')
         assert decision in {'in', 'out', ''}
-        constraint = self.constraints[dim_idx]
         try:
-            self.context, self.constraints = self.coroutine.send((dim_idx, elem, decision))
+            self.user_constraints, self.constraints = self.coroutine.send(self._last_user_input)
+            message = ''
         except StopIteration as last:
-            self.show_found_concept(last.value)
+            self.user_constraints, self.constraints = last.value
+            message = 'FOUND CONCEPT: ' + navigation.pretty_nconcept(self.constraints)
         self.create_widgets()
-
-
-    def show_found_concept(self, concept):
-        """Callback when given concept have been selected by user"""
-        print('FOUND CONCEPT:', navigation.pretty_nconcept(concept), end='\n\n')
+        self.info(message)
 
 
 if __name__ == '__main__':
