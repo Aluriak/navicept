136c5ca44d4bd400acd0b3f4ec152637bb204b11
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-08-23 17:45:44 +0200

    context: pretty print when available + warning to avoid annoying errors

--- context.py
+++ context.py
@@ -19,6 +19,22 @@ class Context(namedtuple('ContextBase', 'sets, relations')):
 
         """
         assert 0. <= density <= 1.
+        # NB: tuple is important: order must be kept in order to get
+        #  consistant ASP program.
         relations = (tuple(rel) for rel in itertools.product(*dimensions)
                      if random.random() < density)
         return Context(dimensions, frozenset(relations))
+
+
+    def __str__(self) -> str:
+        return self.__pretty_print() if len(self.sets) == 2 else repr(self)
+
+    def __pretty_print(self) -> str:
+        ones, twos = self.sets
+        twos = tuple(twos)
+        out = '   ' + '  '.join(twos) + '\n'
+        for one in ones:
+            out += one + '  ' + '  '.join('×' if (one, two) in self.relations else ' ' for two in twos) + '\n'
+        return out
+
+
